24ee9b93fdacecf0bba1416f97bee6af4494b575
Timothy Deibert <timdeibert09@outlook.com>
2026-05-11 14:16:39 -0500

    folder structure stuff

---
+++ Directory_Architecture/structure.py
@@ -0,0 +1,21 @@
+STRUCTURE = {
+    "Projects":{
+        "Nuclear_Scaling":{
+            "Code_Repository":{
+                "Python":{
+                    "Environments": None,
+                    "Model_Training":{
+                        "Training_Scripts": None,
+                        "Trained_Models": None,
+                        "Training_Data": None
+                    },
+                    "Image_Segmentation": None,
+                    "Image_Analysis": None,
+                    "Utility_Scripts": None
+                },
+                "R_Projects": None
+            },
+            "Data": None
+        }
+    }
+}
